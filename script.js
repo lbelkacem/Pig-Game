b0e761c35908b408c10c3c57de1701495a0e4bbb
Lamia Belkacem <lamia.belkacem86@gmail.com>
2023-01-13 09:26:06 -0500

    select elements

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 'use strict';
+//  select elements
+const btnRoll = document.querySelector('.btn--roll');
+const btnNew = document.querySelector('.btn--new');
+const btnHold = document.querySelector('.btn--hold');
+
+const imgDice = document.querySelector('.dice');
+
+//***Player 1***
+const currentEL0 = document.querySelector('.current--0'); //Player 1 ctor('.current--0');
+const scoreEl0 = document.querySelector('.score--0');
+
+//***Player 2***
+const currentEL1 = document.querySelector('.current--1');
+const scoreEl1 = document.querySelector('.score--1');
