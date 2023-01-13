e874ccac0d2ead7153ac1cb9d9eb4dd33b33bc48
Lamia Belkacem <lamia.belkacem86@gmail.com>
2023-01-13 09:50:20 -0500

    if dice different from 1

--- script.js
+++ script.js
@@ -7,15 +7,29 @@ const btnHold = document.querySelector('.btn--hold');
 const imgDice = document.querySelector('.dice');
 
 //***Player 1***
-const currentEL0 = document.querySelector('#current--0'); //Player 1 ctor('.current--0');
+
 const scoreEl0 = document.querySelector('#score--0');
 
 //***Player 2***
-const currentEL1 = document.querySelector('#current--1');
+
 const scoreEl1 = document.querySelector('#score--1');
 
 // initialization
 scoreEl0.textContent = 0;
 scoreEl1.textContent = 0;
+const currentScore = 0;
 
 const activePlayer = 0;
+
+// functions
+const rollDice = function () {
+  const randomNum = Math.random();
+  const dice = Math.ceil(randomNum !== 0 ? randomNum : randomNum + 0.1) * 6;
+  if (dice !== 1) {
+    imgDice.src = `dice-${dice}.png`;
+    currentScore += dice;
+    document.querySelector(`#current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+  }
+};
